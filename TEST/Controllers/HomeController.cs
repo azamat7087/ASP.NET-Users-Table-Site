b249d1ae7c77b114e771ffec48bebc88929d59ea
azaza7087 <azaza7087@gmail.com>
2020-11-29 01:54:52 +0600

    Добавьте файлы проекта.

---
+++ TEST/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TEST.Models;
+
+namespace TEST.Controllers
+{
+    public class HomeController : Controller
+    {
+        UserContext db = new UserContext();
+        public ActionResult Index()
+        {
+
+            IEnumerable<User> users = db.Users;
+            ViewBag.Users = users;
+            
+           
+            return View();
+        }
+
+
+
+
+
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            User u = db.Users.Find(id);
+            if (u == null)
+            {
+                return HttpNotFound();
+            }
+            return View(u);
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id) {
+
+            User u = db.Users.Find(id);
+            if (u != null)
+            {
+                db.Users.Remove(u);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+      
+    }
+}
---
+++ TEST/Models/UserDbInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace TEST.Models
+{
+    public class UserDbInitializer : DropCreateDatabaseAlways<UserContext>
+    {
+        protected override void Seed(UserContext db)
+        {
+            db.Users.Add(new User { First_Name = "Aidanbek", Last_Name = "Kamalov", Age=23, Gender="Male", Country="Kazakhstan", Email="kamalov23@gmail.com" });
+            db.Users.Add(new User { First_Name = "Ruslan", Last_Name = "Olegov", Age = 19, Gender = "Male", Country = "Russia", Email = "rusya228@mail.ru" });
+            db.Users.Add(new User { First_Name = "Helena", Last_Name = "Soretti", Age = 21, Gender = "Female", Country = "Italia", Email = "SsSoretti@gmail.com" });
+
+            base.Seed(db);
+        }
+    }
+}
